93b13eee7b27efd1e9d2a43e0ae2d703776765f5
Ryan Whitty <rwhitty@berkeley.edu>
2023-08-20 16:21:30 -0700

    Improved code readability

--- board.py
+++ board.py
@@ -1,143 +1,205 @@
 import numpy as np
-import pandas as pd
-from collections import defaultdict
+
 
 class Square:
+
     def __init__(self, position, name):
+
         self.position = position
         self.name = name
         self.visits = 0
 
+
 class Property(Square):
+
     def __init__(self, position, name, price):
+
         super().__init__(position, name)
         self.price = price
         self.rent = 0
         self.profit = 0
         self.houses = 0
         self.owner = None
+
     def land_action(self, player):
+
         if not self.owner:
             player.consider(self)
+
         else:
             self.charge(player)
         self.visits += 1
+
     def charge(self, player):
         player.balance -= self.rent
         self.profit += self.rent
         self.owner.balance += self.rent
 
+
 class Building(Property):
+
     def __init__(self, position, name, price, rents, color):
         super().__init__(position, name, price)
         self.rents = rents
         self.color = color
         
+
 class Railroad(Property):
+
     def __init__(self, position, name):
         super().__init__(position, name, 200)
         self.rents = (25, 50, 100, 200)
 
+
 class Utility(Property):
+
     def __init__(self, position, name):
         super().__init__(position, name, 150)
         self.rents = (4, 12)
 
+
 class Tax(Square):
+
     def __init__(self, position, name, cost):
         super().__init__(position, name)
         self.cost = cost
+
     def land_action(self, player):
+
         player.balance -= self.cost
         self.visits += 1
 
+
 class Go(Square):
+
     def __init__(self):
         super().__init__(0, "Go")
+
     def land_action(self, player):
         self.visits += 1
 
+
 class Jail(Square):
+
     def __init__(self):
         super().__init__(10, "Jail")
+
     def land_action(self, player):
+        player.jailed += 1
         self.visits += 1
-        
+
+
 class FreeParking(Square):
+
     def __init__(self):
         super().__init__(20, "Free Parking")
         self.balance = 0
+
     def land_action(self, player):
         player.balance += self.balance
         self.balance = 0
         self.visits += 1
-        
+
+
 class GoToJail(Square):
+
     def __init__(self):
         super().__init__(30, "Go To Jail")
+
     def land_action(self, player):
         player.jailed += 1
         player.update_position(10)
         self.visits += 1
 
+
 class Chance(Square):
+
     def advance_to_boardwalk(player):
         player.update_position(39)
+
     def advance_to_go(player):
         player.update_position(0)
+
     def advance_to_illinois(player):
         player.update_position(24)
+
     def advance_to_railroad_1(player):
+
         if 6 <= player.position <= 15:
             player.update_position(15)
+
         elif 16 <= player.position <= 25:
             player.update_position(25)
+
         elif 26 <= player.position <= 35:
             player.update_position(35)
+
         else:
             player.update_position(5)
+
     def advance_to_railroad_2(player):
+
         if 6 <= player.position <= 15:
             player.update_position(15)
+
         elif 16 <= player.position <= 25:
             player.update_position(25)
+
         elif 26 <= player.position <= 35:
             player.update_position(35)
+
         else:
             player.update_position(5)
+
     def advance_to_reading(player):
         player.update_position(5)
+
     def advance_to_st_charles(player):
         player.update_position(11)
+
     def advance_to_utility(player):
+
         if 13 <= player.position <= 28:
             player.update_position(28)
+
         else:
             player.update_position(12)
+
     def back_three_spaces(player):
         player.update_position((player.position - 3) % 40)
         player.balance -= 200
+
     def bank_pays_dividend(player):
         player.balance += 50
+
     def building_loan_matures(player):
         player.balance += 150
+
     def chairman_of_board(player):
         player.balance -= 150
         board[20].balance += 150
+
     def get_out_of_jail(player):
         player.jailed = -1
+
     def go_to_jail(player):
         player.jailed += 1
         player.update_position(10)
+
     def property_repairs(player):
+
         for prop in player.properties:
+
             if type(prop) == Building:
+
                 if prop.houses < 5:
                     player.balance -= 25*prop.houses
                     board[20].balance += 25*prop.houses
+
                 else:
                     player.balance -= 100
                     board[20].balance += 100
+
     def speeding_fine(player):
         player.balance -= 15
         board[20].balance += 20
@@ -152,59 +214,84 @@ class Chance(Square):
     
     def __init__(self, position, name):
         super().__init__(position, name)
+
     def land_action(self, player):
+
         if not Chance.active_cards:
             Chance.active_cards = list(Chance.all_cards)
+
         active_cards = Chance.active_cards
         card_index = np.random.choice(np.arange(len(active_cards)))
         chosen_card = active_cards.pop(card_index)
         chosen_card(player)
+
         if not active_cards:
             active_cards = self.all_cards
         self.visits += 1
-    
+
+
 class CommunityChest(Square):
+
     def advance_to_go(player):
         player.update_position(0)
+
     def bank_error(player):
         player.balance += 200
+
     def beauty_contest(player):
         player.balance += 10
+
     def birthday(player):
         player.balance += 30
         board[20].balance -= 30
+
     def consultancy_fee(player):
         player.balance += 25
+
     def doctors_fee(player):
         player.balance -= 50
         board[20].balance += 50
+
     def get_out_of_jail(player):
         player.jailed = -1
+
     def go_to_jail(player):
         player.jailed += 1
         player.update_position(10)
+
     def holiday_fund_matures(player):
         player.balance += 100
+
     def hospital_fees(player):
         player.balance -= 100
+
     def income_tax_refund(player):
         player.balance += 20
         board[20].balance += 100
+
     def inherit_money(player):
         player.balance += 100
+
     def life_insurance_matures(player):
         player.balance += 100
+
     def school_fees(player):
         player.balance -= 50
         board[20].balance += 50
+
     def sell_stock(player):
         player.balance += 50
+
     def street_repairs(player):
+
         for prop in player.properties:
+
             if type(prop) == Building:
+
                 if prop.houses < 5:
                     player.balance -= 40*prop.houses
                     board[20].balance += 40*prop.houses
+
                 else:
                     player.balance -= 115
                     board[20].balance += 115
@@ -219,18 +306,24 @@ class CommunityChest(Square):
 
     def __init__(self, position, name):
         super().__init__(position, name)
+
     def land_action(self, player):
+
         if not CommunityChest.active_cards:
             CommunityChest.active_cards = list(CommunityChest.all_cards)
+
         active_cards = CommunityChest.active_cards
         card_index = np.random.choice(np.arange(len(active_cards)))
         chosen_card = active_cards.pop(card_index)
         chosen_card(player)
+
         if not active_cards:
             active_cards = self.all_cards
         self.visits += 1
 
+
 class Player:
+
     def roll():
         die_1 = np.random.choice([1, 2, 3, 4, 5, 6])
         die_2 = np.random.choice([1, 2, 3, 4, 5, 6])
@@ -244,6 +337,7 @@ class Player:
         self.jailed = 0
         
     def consider(self, prop):
+
         if prop.price < self.balance * self.risk:
             self.balance -= prop.price
             prop.owner = self
@@ -252,32 +346,44 @@ class Player:
             prop.rent = prop.rents[0]
     
     def update_position(self, new_pos):
+
         if self.jailed <= 0 and new_pos < self.position:
             self.balance += 200
+
         self.position = new_pos
         board[new_pos].land_action(self)
             
     def take_turn(self):
+
         curr_roll = Player.roll()
+
         if self.jailed:
+
             if curr_roll[0] == curr_roll[1]:
                 self.jailed = 0
+
             elif self.balance * self.risk >= 50 or self.jailed > 3:
                 self.balance -= 50
                 board[20].balance += 50
                 self.jailed = 0
+
             else:
                 self.jailed += 1
+
         else:
+
             self.update_position((self.position + sum(curr_roll)) % 40)
             num_doubles = 1
+
             while curr_roll[0] == curr_roll[1]:
                 num_doubles += 1
+
                 if num_doubles >= 3:
                     self.jailed += 1
                     self.update_position(10)
                     break
                 curr_roll = Player.roll()
+                
             self.update_position((self.position + sum(curr_roll)) % 40)
 
 board = [
